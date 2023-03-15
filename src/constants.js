188ee7526b9ef961d356983bf7b86397907a11c1
Nico Rithner <nicorithner@gmail.com>
2023-03-14 18:12:30 -0600

    Add Constants file with colors

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const PRIMARY = {
+  lightRed: "hsl(0, 100%, 67%)",
+  orangeyYellow: "hsl(39, 100%, 56%)",
+  greenTeal: "hsl(166, 100%, 37%)",
+  cobaltBlue: "hsl(234, 85%, 45%)",
+};
+
+export const GRADIANTS = {
+  LightSlateBlueBackground: "hsl(252, 100%, 67%)",
+  LightRoyalBlueBackground: "hsl(241, 81%, 54%)",
+
+  VioletBlueCircle: "hsla(256, 72%, 46%, 1)",
+  PersianBlueCircle: "hsla(241, 72%, 46%, 0)",
+};
+
+export const NEUTRAL = {
+  White: "hsl(0, 0%, 100%)",
+  PaleBlue: "hsl(221, 100%, 96%)",
+  LightLavender: "hsl(241, 100%, 89%)",
+  DarkGrayBlue: "hsl(224, 30%, 27%)",
+};
