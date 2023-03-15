79445f60b0d7452fc26ef5fedb374b8ce624ebc3
Nico Rithner <nicorithner@gmail.com>
2023-03-15 16:29:05 -0600

    Add SkillBarScore component

--- src/components/resultsSummary/ResultsSummary.jsx
+++ src/components/resultsSummary/ResultsSummary.jsx
@@ -1,55 +1,71 @@
+import SkillScoreBar from "../skillBarScore/SkillScoreBar.jsx";
+import { PRIMARY, PRIMARY_WITH_OPACITY } from "../../constants.js";
 import {
   ResultsSummaryWrapper,
-  TopCard,
+  AvrgScoreCard,
   ScoreCircle,
   Score,
   ScoreSubText,
   SummaryCard,
-  SkillRow,
-  SkillName,
-  SkillRowScore,
+  ScoreBarWrapper,
   SummaryCardButton,
 } from "./ResultsSummary.styled.js";
+
+import iconMemory from "../../assets/images/icon-memory.svg";
+import iconReaction from "../../assets/images/icon-reaction.svg";
+import iconVerbal from "../../assets/images/icon-verbal.svg";
+import iconVisual from "../../assets/images/icon-visual.svg";
 
 export default function ResultsSummary() {
   return (
     <ResultsSummaryWrapper>
-      <TopCard>
-        <h1>Your Result</h1>
-        <ScoreCircle>
-          <Score>76</Score>
-          <ScoreSubText>of 100</ScoreSubText>
-        </ScoreCircle>
-
-        <h1>Great</h1>
-
-        <p>
-          You scored higher than 65% of the people who have taken these tests.
-        </p>
-      </TopCard>
-
       <SummaryCard>
+        <AvrgScoreCard>
+          <h1>Your Result</h1>
+          <ScoreCircle>
+            <Score>76</Score>
+            <ScoreSubText>of 100</ScoreSubText>
+          </ScoreCircle>
+
+          <h1>Great</h1>
+
+          <p>
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </AvrgScoreCard>
+
         <h1>Summary</h1>
 
-        <SkillRow>
-          <SkillName>Reaction</SkillName>
-          <SkillRowScore>80 / 100</SkillRowScore>
-        </SkillRow>
-
-        <SkillRow>
-          <SkillName>Memory</SkillName>
-          <SkillRowScore>92 / 100</SkillRowScore>
-        </SkillRow>
-
-        <SkillRow>
-          <SkillName>Verbal</SkillName>
-          <SkillRowScore>61 / 100</SkillRowScore>
-        </SkillRow>
-
-        <SkillRow>
-          <SkillName>Visual</SkillName>
-          <SkillRowScore>72 / 100</SkillRowScore>
-        </SkillRow>
+        <ScoreBarWrapper>
+          <SkillScoreBar
+            name="Reaction"
+            score={80}
+            backgroundColor={PRIMARY_WITH_OPACITY.lightRed}
+            color={PRIMARY.lightRed}
+            icon={iconReaction}
+          />
+          <SkillScoreBar
+            name="Memory"
+            score={92}
+            backgroundColor={PRIMARY_WITH_OPACITY.orangeyYellow}
+            color={PRIMARY.orangeyYellow}
+            icon={iconMemory}
+          />
+          <SkillScoreBar
+            name="Verbal"
+            score={61}
+            backgroundColor={PRIMARY_WITH_OPACITY.greenTeal}
+            color={PRIMARY.greenTeal}
+            icon={iconVerbal}
+          />
+          <SkillScoreBar
+            name="Visual"
+            score={72}
+            backgroundColor={PRIMARY_WITH_OPACITY.cobaltBlue}
+            color={PRIMARY.cobaltBlue}
+            icon={iconVisual}
+          />
+        </ScoreBarWrapper>
 
         <SummaryCardButton>Continue</SummaryCardButton>
       </SummaryCard>
--- src/components/resultsSummary/ResultsSummary.styled.js
+++ src/components/resultsSummary/ResultsSummary.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ResultsSummaryWrapper = styled.div`
 
 `
-export const TopCard = styled.div`
+export const AvrgScoreCard = styled.div`
 
 `
 export const ScoreCircle = styled.div`
@@ -18,15 +18,9 @@ export const ScoreSubText = styled.div`
 export const SummaryCard = styled.div`
 
 `
-export const SkillRow = styled.div`
+export const ScoreBarWrapper = styled.div`
 
 `
-export const SkillName = styled.div`
-
-`
-export const SkillRowScore = styled.div`
-
-`
-export const SummaryCardButton = styled.div`
+export const SummaryCardButton = styled.button`
 
 `
