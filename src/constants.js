b54205e096dc09cac664751d944809d66d846496
Nico Rithner <nicorithner@gmail.com>
2023-03-15 16:29:47 -0600

    Add DarkBlue and opacity variations to match Figma

--- src/constants.js
+++ src/constants.js
@@ -3,19 +3,28 @@ export const PRIMARY = {
   orangeyYellow: "hsl(39, 100%, 56%)",
   greenTeal: "hsl(166, 100%, 37%)",
   cobaltBlue: "hsl(234, 85%, 45%)",
+  darkNavyBlue: "hsl(223, 30%, 27%)"
+};
+
+export const PRIMARY_WITH_OPACITY = {
+  lightRed: "hsla(0, 100%, 67%, 0.15)",
+  orangeyYellow: "hsla(39, 100%, 56%, 0.15)",
+  greenTeal: "hsla(166, 100%, 37%, 0.15)",
+  cobaltBlue: "hsla(234, 85%, 45%, 0.15)",
 };
 
 export const GRADIANTS = {
-  LightSlateBlueBackground: "hsl(252, 100%, 67%)",
-  LightRoyalBlueBackground: "hsl(241, 81%, 54%)",
+  lightSlateBlueBackground: "hsl(252, 100%, 67%)",
+  lightRoyalBlueBackground: "hsl(241, 81%, 54%)",
 
-  VioletBlueCircle: "hsla(256, 72%, 46%, 1)",
-  PersianBlueCircle: "hsla(241, 72%, 46%, 0)",
+  violetBlueCircle: "hsla(256, 72%, 46%, 1)",
+  persianBlueCircle: "hsla(241, 72%, 46%, 0)",
+  darkNavyBlue: "hsla(223, 30%, 27%, 0.5)"
 };
 
 export const NEUTRAL = {
-  White: "hsl(0, 0%, 100%)",
-  PaleBlue: "hsl(221, 100%, 96%)",
-  LightLavender: "hsl(241, 100%, 89%)",
-  DarkGrayBlue: "hsl(224, 30%, 27%)",
+  white: "hsl(0, 0%, 100%)",
+  paleBlue: "hsl(221, 100%, 96%)",
+  lightLavender: "hsl(241, 100%, 89%)",
+  darkGrayBlue: "hsl(224, 30%, 27%)",
 };
