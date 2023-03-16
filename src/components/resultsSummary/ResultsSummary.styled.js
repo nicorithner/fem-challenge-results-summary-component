6aff886c9db7372348385c3888f39f6965e1f100
Nico Rithner <nicorithner@gmail.com>
2023-03-16 17:08:03 -0600

    Add initial styling to main wrapper

--- src/components/resultsSummary/ResultsSummary.styled.js
+++ src/components/resultsSummary/ResultsSummary.styled.js
@@ -1,26 +1,15 @@
 import styled from "styled-components";
 
 export const ResultsSummaryWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  width: 736px;
+  height: 512px;
+`;
 
-`
-export const AvrgScoreCard = styled.div`
-
-`
-export const ScoreCircle = styled.div`
-
-`
-export const Score = styled.div`
-
-`
-export const ScoreSubText = styled.div`
-
-`
-export const SummaryCard = styled.div`
-
-`
-export const ScoreBarWrapper = styled.div`
-
-`
-export const SummaryCardButton = styled.button`
-
-`
+export const SummaryCard = styled.div``;
+export const ScoreBarWrapper = styled.div``;
+export const SummaryCardButton = styled.button``;
