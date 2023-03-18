868d7c01a518e12fa01b468b8c419ea8c666088c
Nico Rithner <nicorithner@gmail.com>
2023-03-18 13:02:26 -0600

    Add Wrapper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import GlobalStyles from "./GlobalStyles";
 import ResultsSummary from "./components/resultsSummary/ResultsSummary";
+import styled from "styled-components";
+import { NEUTRAL } from "./constants";
 
 function App() {
   return (
-    <>
+    <Wrapper>
       <ResultsSummary />
       <GlobalStyles />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  background-color: ${NEUTRAL.paleBlue};
+
+  @media (min-width: 400px) {
+    // height: 100%;
+  }
+`;
 
 export default App;
